e4f1e72e766358c5e267ac4b063229d86f3ade4c
anastasiyakamashyla <anastasiya.komashilo@gmail.com>
2018-10-14 22:15:55 +0300

    Commit Coding Practice Tasks update

--- src/by/task3/Formula3.java
+++ src/by/task3/Formula3.java
@@ -8,17 +8,29 @@ public class Formula3 {
 		double a = readFromConcole("Введите a:");
 		double b = readFromConcole("Введите b:");
 		double h = readFromConcole("Введите h:");
-		double fx;
-		double x;
-		if (a < b) {
-			
-			for (x = a; x <= b; x += h) {
-				fx = calcFx(x);
-				printResult(x, fx);
-			}
-		} else {
+		int checkRun = runFxFromAtoB(a, b, h);
+		if (checkRun == -1) {
 			System.out.print("Верхняя граница должна быть меньше нижней границы диапазона");
 		}
+		if (checkRun == -2) {
+			System.out.print("Шаг должен быть больше нуля");
+		}
+	}
+
+	public static int runFxFromAtoB(double a, double b, double h) {
+		double x;
+		double fx;
+		if (a >= b) {
+			return -1;
+		}
+		if (h <= 0) {
+			return -2;
+		}
+		for (x = a; x <= b; x += h) {
+			fx = calcFx(x);
+			printResult(x, fx);
+		}
+		return 0;
 	}
 
 	private static double readFromConcole(String printLine) {
@@ -46,4 +58,3 @@ public class Formula3 {
 	}
 
 }
- 
--- test/task3/Formula3Test.java
+++ test/task3/Formula3Test.java
@@ -14,4 +14,31 @@ public class Formula3Test {
 		double expectedY = 0.9951;
 		Assert.assertEquals(expectedY, realY,  0.0001);
 	}
+	@Test
+	public void testrunFxBLessA() {
+		double a = 9;
+		double b = 3;
+		double h = 1;
+		int realFx = Formula3.runFxFromAtoB(a, b, h);
+		int expectedFx = -1;
+		Assert.assertEquals(expectedFx, realFx);
+	}
+	@Test
+	public void testrunFxBEqualsA() {
+		double a = 3;
+		double b = 3;
+		double h = 1;
+		int realFx = Formula3.runFxFromAtoB(a, b, h);
+		int expectedFx = -1;
+		Assert.assertEquals(expectedFx, realFx);
+	}
+	@Test
+	public void testrunFxHInvalid() {
+		double a = 3;
+		double b = 9;
+		double h = 0;
+		int realFx = Formula3.runFxFromAtoB(a, b, h);
+		int expectedFx = -2;
+		Assert.assertEquals(expectedFx, realFx);
+	}
 }
